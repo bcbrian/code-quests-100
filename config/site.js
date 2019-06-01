a0e19f0cbd1374ae68d37cf67e8cc2178d972dd2
Brian Bartholomew <me@bcbrian.com>
2019-05-31 21:12:58 -0500

    Update site.js

--- config/site.js
+++ config/site.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  title: 'Code Quests', // Navigation and Site Title
+  titleAlt: '100 Days to Code', // Title for JSONLD
+  description: 'A guide to learn how to code in 100 days.',
+  url: 'https://100.codequests.io', // Domain of your site. No trailing slash!
+  siteUrl: 'https://100.codequests.io', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
-  themeColor: '#3e7bf2',
+  shortName: 'CodeQuests', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Brian', // Author for schemaORGJSONLD
+  themeColor: '#663399',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
+  twitter: '@thebcbrian', // Twitter Username
 };
